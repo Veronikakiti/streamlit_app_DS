24a595594fc8d0611972381c47d7dcb8ea9c40e6
Veronikakiti <93826577+Veronikakiti@users.noreply.github.com>
2024-04-08 12:47:42 +0300

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from PIL import Image
-from pickle import dump, load
+
 st.set_page_config(
         layout="wide",
         initial_sidebar_state="auto",
@@ -12,6 +12,55 @@ st.set_page_config(
 
 st.header("Предсказание кредитоспособности клиента")
 st.subheader("Определим, одобрит ли банк Вам кредит или нет")
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from pickle import dump, load
+
+
+def open_data(path="data/data_loan"):
+    df = pd.read_csv('data/data_loan.csv')
+    df = df[['Gender', 'Married', 'Dependents',
+        'Education', 'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome',
+        'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area',
+        'Loan_Status']]
+    return df
+
+
+def split_data(df):
+    y_df = df['Loan_Status']
+    X_df = df.drop(columns=['Loan_Status'])
+    X_train, X_test, y_train, y_test = train_test_split(X_df, y_df, test_size=0.1, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+def preprocess_data(df):
+    df = df.dropna()
+    binary_cols = ['Married', 'Loan_Status']
+    categ_cols = ['Gender', 'Dependents', 'Education', 'Self_Employed', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']
+
+    for col in categ_cols:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+    for col in binary_cols:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+
+    X_train, X_test, y_train, y_test = split_data(df)
+    return X_train, X_test, y_train, y_test
+
+
+def fit_and_save_model(X_train, X_test, y_train, y_test, path='data/model_weights.mw'):
+    model = RandomForestClassifier(n_estimators=100, max_depth=3, min_samples_leaf=10, random_state=42)
+    model.fit(X_train, y_train)
+    test_prediction = model.predict(X_test)
+    accuracy = accuracy_score(test_prediction, y_test)
+    print(f"Model accuracy is {accuracy:.2f}")
+    with open(path, "wb") as file:
+        dump(model, file)
+    print(f"Model was saved to {path}")
 
 def load_model_and_predict(df, path='data/model_weights.mw'):
     with open(path, "rb") as file:
@@ -32,6 +81,13 @@ def load_model_and_predict(df, path='data/model_weights.mw'):
     prediction_df = pd.DataFrame(prediction_data, index=[0])
     prediction = encode_prediction[prediction]
     return prediction, prediction_df
+
+
+if __name__ == "__main__":
+    df = open_data()
+    X_train, X_test, y_train, y_test = preprocess_data(df)
+    fit_and_save_model(X_train, X_test, y_train, y_test)
+
 
 def process_main_page():
     image = Image.open('data/банк.jpg')
