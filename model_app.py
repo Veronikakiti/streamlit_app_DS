a93c628b645a18150a91cf6dcd8e1c808bdc0004
Veronikakiti <93826577+Veronikakiti@users.noreply.github.com>
2024-04-08 12:34:13 +0300

    Update model_app.py

--- model_app.py
+++ model_app.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import streamlit as st
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
